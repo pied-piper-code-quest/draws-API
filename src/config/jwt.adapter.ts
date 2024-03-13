cc0880652648b3cf0724483026b29480501dd15e
Aarón Valdez <aaronvaldez2001@gmail.com>
2024-03-13 17:07:23 -0400

    fit JWT payload type

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -3,10 +3,16 @@ import { envs } from "./envs";
 
 const JWT_SEED = envs.JWT_SECRET_KEY;
 
+interface TokenPayload<T> {
+  payload: T;
+  iat: number;
+  exp: number;
+}
+
 export class JwtAdapter {
   static async generateToken(
     payload: Object,
-    duration: string = "2h",
+    duration: string = "7d",
   ): Promise<string | null> {
     return new Promise(resolve => {
       jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
@@ -16,12 +22,17 @@ export class JwtAdapter {
     });
   }
 
-  static verifyToken<T = { id: string }>(token: string): Promise<T | null> {
+  static verifyToken<T = { id: string }>(
+    token: string,
+  ): Promise<TokenPayload<T> | null> {
     return new Promise(resolve => {
-      jwt.verify(token, JWT_SEED, (err, decoded) => {
+      jwt.verify(token, JWT_SEED, (err, decoded: any) => {
         if (err || !decoded) return resolve(null);
-        resolve(decoded as T);
+        resolve(decoded as TokenPayload<T>);
       });
     });
+  }
+  static decode(token: string) {
+    return jwt.decode(token) as any | null;
   }
 }
