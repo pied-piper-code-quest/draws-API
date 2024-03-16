e3143377e5d6aab67d24ac02d4e30eb146dc72e9
Aarón Valdez <aaronvaldez2001@gmail.com>
2024-03-15 20:40:32 -0400

    Update user-admin.mapper.ts

--- src/infrastructure/mappers/user-admin.mapper.ts
+++ src/infrastructure/mappers/user-admin.mapper.ts
@@ -5,13 +5,17 @@ export class UserAdminMapper {
   static UserAdminEntityFromObject(
     props: Record<string, any>,
   ): UserAdminEntity {
-    const { id, _id, username, email, isActive, createdAt, updatedAt } = props;
+    const { id, _id, username, name, email, isActive, createdAt, updatedAt } =
+      props;
 
     if (!id && !_id) {
       throw CustomError.badRequest("Falta id");
     }
     if (!username) {
       throw CustomError.badRequest("Falta username");
+    }
+    if (!name) {
+      throw CustomError.badRequest("Falta name");
     }
     if (!email) {
       throw CustomError.badRequest("Falta email");
@@ -27,6 +31,7 @@ export class UserAdminMapper {
     // }
     return new UserAdminEntity(
       id || _id,
+      name,
       username,
       email,
       // password,
